0f54c164117fad5862f2123e98600b9feb2747f6
Criogen11 <tsv1985@bk.ru>
2017-10-09 20:01:29 +0300

    Init database make @db

--- app.rb
+++ app.rb
@@ -1,17 +1,33 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+#  Создание подключения к базе
+def init_db
+	#  Создаем глобальную переменную которая ссылается на базу данных Leprosorium.db
+	@db = SQLite3::Database.new 'leprosorium.db'
+	# Включаем функцию которая возвращает результат запроса в виде хеша а не в виде массива
+	@db.results_as_hash = true
+end	
+
+#   Данная фенкция позволяет видеть все что в ней находится при каждом обращении GET или POST
+before do
+
+end
 
 get '/' do
-  erb 'Can you handle a <a href="/secure/place">secret</a>?'
+	erb 'Can you handle a <a href="/secure/place">secret</a>?'
 end
 
 get '/new' do
-  erb :new
+	erb :new
 end
 
 post '/new' do
-  content = params['content']
-  erb "Вы ввели #{content}"
+
+
+	content = params['content']
+
 end
 
